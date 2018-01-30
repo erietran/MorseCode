a3cdd12453d650d183a172f32667c1e1ee49ed9b
EricTlap\Eric <erietran@ut.utm.edu>
2018-01-30 15:51:48 -0600

    Fixed placement of functions, part 1 turn in.

---
+++ app/src/main/java/com/example/eric/morsecode/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.eric.morsecode
+
+import android.app.Activity
+import android.content.Context
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.text.method.ScrollingMovementMethod
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        mtextView.movementMethod = ScrollingMovementMethod()
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        testButton.setOnClickListener { view ->
+            appendTextAndScroll(inputText.text.toString());
+            hideKeyboard();
+        }
+    }
+    private fun appendTextAndScroll(text: String) {
+        if(mtextView!= null) {
+            mtextView.append(text + "\n")
+            val layout = mtextView.getLayout()
+            if (layout != null){
+                val scrollDelta = (layout!!.getLineBottom( mtextView.getLineCount() - 1)
+                        - mtextView.getScrollY() - mtextView.getHeight());
+                if (scrollDelta > 0)
+                    mtextView.scrollBy( 0, scrollDelta);
+
+            }
+        }
+    }
+    fun Activity.hideKeyboard() {
+        hideKeyboard(if(currentFocus == null) View( this) else currentFocus)
+    }
+
+    fun Context.hideKeyboard(view: View){
+        val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+}
+
+
+
+
+
