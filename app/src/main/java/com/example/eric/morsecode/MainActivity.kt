ddff28577ab1b0603bee4fe115335d4de950c93a
EricTlap\Eric <erietran@ut.utm.edu>
2018-02-03 20:37:37 -0600

    Part 2 complete

--- app/src/main/java/com/example/eric/morsecode/MainActivity.kt
+++ app/src/main/java/com/example/eric/morsecode/MainActivity.kt
@@ -4,10 +4,12 @@ import android.app.Activity
 import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.text.TextUtils.lastIndexOf
 import android.text.method.ScrollingMovementMethod
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
 
 
 class MainActivity : AppCompatActivity() {
@@ -21,7 +23,31 @@ class MainActivity : AppCompatActivity() {
             appendTextAndScroll(inputText.text.toString());
             hideKeyboard();
         }
+        buildDictsWithJSON(jsonObj = loadMorseJSON())
+        showButton.setOnClickListener{
+            showCodes()
+            hideKeyboard()
+        }
     }
+
+
+    var letToCodeDict: HashMap<String, String> = HashMap();
+    var codeToLetDict: HashMap<String, String> = HashMap();
+
+    fun buildDictsWithJSON(jsonObj : JSONObject){
+        for(k in jsonObj.keys()){
+            val code = jsonObj.getString(k)
+            letToCodeDict.put(k, code)
+            codeToLetDict.put(code, k)
+        }
+    }
+
+    fun showCodes(){
+        appendTextAndScroll("HERE ARE THE CODES")
+        for(k in letToCodeDict.keys.sorted())
+            appendTextAndScroll("$k: ${letToCodeDict[k]}")
+    }
+
     private fun appendTextAndScroll(text: String) {
         if(mtextView!= null) {
             mtextView.append(text + "\n")
@@ -34,6 +60,14 @@ class MainActivity : AppCompatActivity() {
 
             }
         }
+    }
+    fun loadMorseJSON() : JSONObject {
+        val filePath = "morse.json"
+        val jsonStr = application.assets.open(filePath).bufferedReader().use{
+            it.readText()
+        }
+        val jsonObj = JSONObject(jsonStr.substring(jsonStr.indexOf("{"), jsonStr.lastIndexOf("}") + 1))
+        return jsonObj;
     }
     fun Activity.hideKeyboard() {
         hideKeyboard(if(currentFocus == null) View( this) else currentFocus)
